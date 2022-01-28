e56b2e10a55ccc39cfb2bce5d9597ad04a67035f
cloudlee <cloudlee@MacBook-Pro-9.local>
2022-01-28 13:50:38 +0900

    [Modify] Todo > 주석 변경

--- pagination/src/components/Pagination.js
+++ pagination/src/components/Pagination.js
@@ -2,12 +2,13 @@
 
 export default function Pagination({ app, initialState }) {
   this.state = initialState;
-  const pageViewCount = 10;
-  let totalPages = Math.ceil(this.state.totalItemCount / this.pagePerItemCount); //총 페이지 수
-  let pageGroup = Math.ceil(this.state.currentPage / pageViewCount); // 화면에 보여질 페이지 그룹
-  let last = pageGroup * 10; // 화면에 보여질 마지막 값
-  if (last > totalPages) last = totalPages; // 총 페이지를 넘어가지 않기 위한 조건
-  let start = last - (pageViewCount - 1) <= 0 ? 1 : last - (10 - 1); //화면에 보여질 첫번째 값
+  const pageViewCount = 10; // 페이지당 보여주고 싶은 아이템 개수
+  let totalPages = Math.ceil(this.state.totalItemCount / this.pagePerItemCount); //총 페이지 수 = 전체아이템개수 / 한페이지당 아이템 개수
+
+  let pageGroup = Math.ceil(this.state.currentPage / pageViewCount); // 화면에 보여질 페이지 그룹 ex) 1그룹 : 1.-5, 2그룹: 6-10 ... = 올림(현재페이지/ 보여줄 페이지그룹당 페이지 개수)
+  let last = pageGroup * 10; // 화면에 보여질 마지막 값 = 현재페이지 그룹 * 현재 페이지
+  if (last > totalPages) last = totalPages; // 토탈보다 크면 토탈페이지로
+  let start = last - (pageViewCount - 1) <= 0 ? 1 : last - (10 - 1); //화면에 보여질 첫번째 값 = 현재 페이지그룹 마지막값 - (페이지당 보여줄 개수 값 - 1) <=0 ? 1:  마지막값 - (페이지당 보여줄 페이지개수값 - 1)
 
   let totalPagesArr = [];
   // let end =
@@ -21,9 +22,10 @@ export default function Pagination({ app, initialState }) {
     //if variable exist update here
     let totalPages = Math.ceil(nextState.totalItemCount / nextState.rItemCount);
     let pageGroup = Math.ceil(nextState.currentPage / pageViewCount);
-    let last = pageGroup * 10;
+    let last = pageGroup * pageViewCount;
     if (last > totalPages) last = totalPages;
-    let start = last - (pageViewCount - 1) <= 0 ? 1 : last - (10 - 1);
+    let start =
+      last - (pageViewCount - 1) <= 0 ? 1 : last - (pageViewCount - 1);
 
     //초기화 후 재 생성
     totalPagesArr = [];
