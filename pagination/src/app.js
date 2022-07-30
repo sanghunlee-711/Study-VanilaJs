4cf51f3a464e91dae04c08d5fbbdf018f6d9f39d
cloudlee <cloudlee@MacBook-Pro-9.local>
2022-07-31 08:21:29 +0900

    [Modify] Pagination to comonent concept

--- pagination/src/app.js
+++ pagination/src/app.js
@@ -1,15 +1,29 @@
 import Pagination from './components/Pagination.js';
 
-export default function App(app) {
-  this.state = {
-    currentPage: 1,
-    totalItemCount: 300,
-    pagePerItemCount: 20,
+export default function App({ target, initialState }) {
+  this.target = target;
+  this.state = initialState;
+  this.setState = function (nextState) {
+    this.state = nextState;
+    pagination.setState(this.state);
   };
 
-  const pagination = new Pagination({ app, initialState: this.state });
+  this.onNext = function (e) {
+    console.log('CLICK NEXT');
+    const nextState = { ...this.state, currentPage: ++this.state.currentPage };
+    this.setState(nextState);
+  };
 
-  pagination.setState({
-    currentPage: 7,
+  this.onPrev = function (e) {
+    console.log('CLICK PREV');
+    const nextState = { ...this.state, currentPage: --this.state.currentPage };
+    this.setState(nextState);
+  };
+
+  const pagination = new Pagination({
+    target: this.target,
+    initialState: this.state,
+    onNext: this.onNext.bind(this),
+    onPrev: this.onPrev.bind(this),
   });
 }
--- pagination/src/components/Pagination.js
+++ pagination/src/components/Pagination.js
@@ -1,47 +1,49 @@
 // ref : https://velog.io/@eunoia/JS%EB%A1%9C-Pagination-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
 
-export default function Pagination({ app, initialState }) {
+export default function Pagination({ target, initialState, onNext, onPrev }) {
+  this.target = target;
   this.state = initialState;
-  const pageViewCount = 10; // 페이지당 보여주고 싶은 아이템 개수
-  let totalPages = Math.ceil(this.state.totalItemCount / this.pagePerItemCount); //총 페이지 수 = 전체아이템개수 / 한페이지당 아이템 개수
+  this.totalPagesArr;
+  const pageViewCount = 10;
+  this.pages = {
+    totalPages: Math.ceil(this.state.totalItemCount / this.pagePerItemCount), //총 페이지 수 = 전체아이템개수 / 한페이지당 아이템 개수
+    pageGroup: Math.ceil(this.state.currentPage / pageViewCount), // 화면에 보여질 페이지 그룹 ex) 1그룹 : 1.-5, 2그룹: 6-10 ... = 올림(현재페이지/ 보여줄 페이지그룹당 페이지 개수)
+    last: Math.ceil(this.state.currentPage / pageViewCount) * 10,
+    start:
+      Math.ceil(this.state.currentPage / pageViewCount) * 10 -
+        (pageViewCount - 1) <=
+      0
+        ? 1
+        : Math.ceil(this.state.currentPage / pageViewCount) * 10 - (10 - 1),
+  };
 
-  let pageGroup = Math.ceil(this.state.currentPage / pageViewCount); // 화면에 보여질 페이지 그룹 ex) 1그룹 : 1.-5, 2그룹: 6-10 ... = 올림(현재페이지/ 보여줄 페이지그룹당 페이지 개수)
-  let last = pageGroup * 10; // 화면에 보여질 마지막 값 = 현재페이지 그룹 * 현재 페이지
-  if (last > totalPages) last = totalPages; // 토탈보다 크면 토탈페이지로
-  let start = last - (pageViewCount - 1) <= 0 ? 1 : last - (10 - 1); //화면에 보여질 첫번째 값 = 현재 페이지그룹 마지막값 - (페이지당 보여줄 개수 값 - 1) <=0 ? 1:  마지막값 - (페이지당 보여줄 페이지개수값 - 1)
-
-  let totalPagesArr = [];
-  // let end =
-
-  for (let i = start; i <= last; i++) {
-    totalPagesArr.push(i);
-  }
+  let totalPagesArr = Array.from(
+    { length: this.pages.last - this.pages.start },
+    (_, i) => i + 1
+  );
 
   this.setState = (nextState) => {
-    this.state = { ...this.state, ...nextState };
-    //if variable exist update here
-    let totalPages = Math.ceil(nextState.totalItemCount / nextState.rItemCount);
-    let pageGroup = Math.ceil(nextState.currentPage / pageViewCount);
-    let last = pageGroup * pageViewCount;
-    if (last > totalPages) last = totalPages;
-    let start =
-      last - (pageViewCount - 1) <= 0 ? 1 : last - (pageViewCount - 1);
-
-    //초기화 후 재 생성
-    totalPagesArr = [];
-    for (let i = start; i <= last; i++) {
-      totalPagesArr.push(i);
-    }
-
+    console.log('set state in comp', { nextState });
+    this.state = nextState;
     this.render();
   };
 
+  const wrapper = document.createElement('section');
+  wrapper.setAttribute('class', 'container');
+  wrapper.innerHTML = `
+  <button id="decrease">&larr;</button>
+    <div id="page-wrapper">
+    </div>
+  <button id="increase">&rarr;</button>`;
+
+  this.target.appendChild(wrapper);
+
   this.render = () => {
-    app.innerHTML = `
-    <section class="container">
-    <button id="decrease">&larr;</button>
-      <div>
-        ${totalPagesArr
+    document.querySelector('#page-wrapper').innerHTML = `
+        ${Array.from(
+          { length: this.pages.last - this.pages.start },
+          (_, i) => i + 1
+        )
           .map(
             (el) => `
           <span class="${
@@ -50,11 +52,16 @@ export default function Pagination({ app, initialState }) {
         `
           )
           .join(' ')}
-      </div>
-      <button id="increase">&rarr;</button>
-      </section>
     `;
   };
+
+  document.querySelector('#decrease').addEventListener('click', (e) => {
+    onPrev(e);
+  });
+
+  document.querySelector('#increase').addEventListener('click', (e) => {
+    onNext(e);
+  });
 
   this.render();
 }
