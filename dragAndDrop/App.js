6b557feda71db1f2c904dda7f427f5d97b5fc86a
cloudlee <cloudlee@MacBook-Pro-9.local>
2022-08-02 16:48:19 +0900

    [Add] Drag and Drop Logic

--- dragAndDrop/App.js
+++ dragAndDrop/App.js
@@ -6,6 +6,7 @@ const CONATINER_MAP = ['In Progress', 'Paused', 'Under Review', 'Completed'];
 const App = function ({ $target, initialData }) {
   this.data = initialData;
   this.$target = $target;
+  this.dragTarget = { value: '', step: '' };
 
   this.handleListState = ({ value, step }) => {
     const newData = this.data.map((el) => {
@@ -18,10 +19,10 @@ const App = function ({ $target, initialData }) {
         return el;
       }
     });
-    console.log(value, step, newData);
+    console.log('new DAta', newData);
 
     this.data = newData;
-    // column.setState(this.data);
+
     allTaskColumn.setState(this.data);
     inProgresscolumn.setState(this.data);
     pausedColumn.setState(this.data);
@@ -29,42 +30,73 @@ const App = function ({ $target, initialData }) {
     completedColumn.setState(this.data);
   };
 
+  this.setDragTarget = ({ value, step }) => {
+    this.dragTarget = { value, step };
+  };
+
+  this.onDrag = (e) => {
+    console.log('onDrag!', e.target.dataset);
+    this.dragTarget = {
+      value: e.target.textContent,
+      step: e.target.dataset.id,
+    };
+  };
+
+  this.onDrop = (e) => {
+    const step = e.target.closest('ul').dataset.id;
+    //step이 유효한 값이면 업데이트하깅(현재랑 다르고 스텝 배열안에 값이 있는경우)
+
+    if (step === this.dragTarget.step) return;
+    this.handleListState({
+      value: this.dragTarget.value,
+      step,
+    });
+  };
+
   const container = new Container({
     $target: this.$target,
   });
+
   const containerTarget = document.querySelector('.container');
+
   const allTaskColumn = new Column({
     initialTitle: 'All Tasks',
     $target: containerTarget,
     data: this.data,
-    handleListState: this.handleListState,
+    onDrag: this.onDrag,
+    onDrop: this.onDrop,
   });
 
   const inProgresscolumn = new Column({
     initialTitle: 'In Progress',
     $target: containerTarget,
     data: this.data,
-    handleListState: this.handleListState,
+
+    onDrag: this.onDrag,
+    onDrop: this.onDrop,
   });
   const pausedColumn = new Column({
     initialTitle: 'Paused',
     $target: containerTarget,
     data: this.data,
-    handleListState: this.handleListState,
+    onDrag: this.onDrag,
+    onDrop: this.onDrop,
   });
 
   const underReviewColumn = new Column({
     initialTitle: 'Under Review',
     $target: containerTarget,
     data: this.data,
-    handleListState: this.handleListState,
+    onDrag: this.onDrag,
+    onDrop: this.onDrop,
   });
 
   const completedColumn = new Column({
     initialTitle: 'Completed',
     $target: containerTarget,
     data: this.data,
-    handleListState: this.handleListState,
+    onDrag: this.onDrag,
+    onDrop: this.onDrop,
   });
 };
 
--- dragAndDrop/components/Column.js
+++ dragAndDrop/components/Column.js
@@ -1,10 +1,11 @@
-const Column = function ({ initialTitle, $target, data, handleListState }) {
+const Column = function ({ initialTitle, $target, data, onDrag, onDrop }) {
   this.data = data;
   this.$target = $target;
   this.title = initialTitle;
   this.dragTarget = null;
 
-  const wrapper = document.createElement('div');
+  const wrapper = document.createElement('ul');
+  wrapper.setAttribute('data-id', this.title);
   wrapper.setAttribute('class', 'column');
 
   this.$target.appendChild(wrapper);
@@ -25,7 +26,7 @@ const Column = function ({ initialTitle, $target, data, handleListState }) {
         .filter((el) => el.step === this.title)
         .map((el) => {
           return `
-          <div class="item" draggable="true">${el.value}</div>
+          <li class="item" draggable="true" data-id=${this.title}>${el.value}</li>
         `;
         })
         .join('')}
@@ -34,52 +35,16 @@ const Column = function ({ initialTitle, $target, data, handleListState }) {
 
   this.render();
 
-  const dragStart = (e) => {
-    console.log('drag started');
-    this.setDragTarget(e.target);
-    setTimeout(() => (e.target.className = 'invisible'), 0);
-  };
-
-  const dragEnd = (e) => {
-    console.log('drag ended');
-    e.target.className = 'item';
-  };
-
-  const dragOver = (e) => {
-    //drap dropped이벤트를 발생시키기 위해 기본 event를 막아줌
+  this.$target.addEventListener('dragstart', (e) => {
+    if (e.target.className !== 'item') return;
+    onDrag(e);
+  });
+  this.$target.addEventListener('dragover', (e) => {
     e.preventDefault();
-    console.log('drag over');
-  };
-  const dragEnter = (e) => {
-    console.log('drag entered');
-  };
-  const dragLeave = (e) => {
-    console.log('drag left');
-  };
-  const dragDrop = (e) => {
-    console.log('drag dropped');
-    // console.log(e.target.textContent.trim(), this.dragTarget.textContent);
-
-    const step = e.target.querySelector('h1').textContent;
-
-    handleListState({
-      value: this.dragTarget.textContent,
-      step,
-    });
-    // this.setDragTarget(null);
-    // this.setState([...this.data, ...e.target.textContent]);
-  };
-
-  document.querySelectorAll('.item').forEach((item) => {
-    item.addEventListener('dragstart', dragStart);
-    item.addEventListener('dragend', dragEnd);
   });
 
-  document.querySelectorAll('.column').forEach((column) => {
-    column.addEventListener('dragover', (e, idx) => dragOver(e, idx));
-    column.addEventListener('dragenter', (e, idx) => dragEnter(e, idx));
-    column.addEventListener('dragleave', (e, idx) => dragLeave(e, idx));
-    column.addEventListener('drop', (e, idx) => dragDrop(e, idx));
+  this.$target.addEventListener('drop', (e) => {
+    onDrop(e);
   });
 };
 
